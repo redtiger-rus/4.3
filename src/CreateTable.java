8838da21821b5b1e65272230b832f612be4b68b5
redtiger-rus <info@redtiger.ru>
2021-03-10 14:11:17 +0300

    Работа со статическими классами.

--- src/CreateTable.java
+++ src/CreateTable.java
@@ -2,47 +2,27 @@ import java.sql.*;
 
 
 public class CreateTable {
-
     public static void main(String args[]) {
-
         Connection c = null;
-
         Statement stmt = null;
-
         String CreateSql = null;
 
         try {
-
             Class.forName("org.postgresql.Driver");
-
             c = DriverManager.getConnection(
-
-                    "jdbc:postgresql://192.168.43.141:5433/rt_db",
-
-                    "redtiger", "1q2w3e4r5t");
+                    "jdbc:postgresql://192.168.1.67:5432/rt_db",
+                    "test_user", "1q2w3e4r5t");
 
             System.out.println("Database Connected ..");
-
             stmt = c.createStatement();
-
-            CreateSql = "Create Table Test(id int primary key, name varchar, address text) ";
-
+//            CreateSql = "Create Table Test(id int primary key, name varchar, address text) ";
             stmt.executeUpdate(CreateSql);
-
             stmt.close();
-
             c.close();
-
         } catch (Exception e) {
-
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
-
             System.exit(0);
-
         }
-
-        System.out.println("Table Created successfully");
-
+//        System.out.println("Table Created successfully");
     }
-
 }
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        String affirmation = VariableClass.iwont + " looser, " + VariableClass.iwill + " developer";
+        System.out.println(affirmation);
+    }
+    static class VariableClass{
+        static String iwont ="I won't";
+        static String iwill ="I will";
+    }
+}
